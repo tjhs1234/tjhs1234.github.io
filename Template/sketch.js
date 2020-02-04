b6485613e3c3f7e9cba07cb2e01a451cd26cf95b
tjhs1234 <60493051+tjhs1234@users.noreply.github.com>
2020-02-04 14:10:25 -0600

    First Demo Complete

--- Template/sketch.js
+++ Template/sketch.js
@@ -5,11 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+let r = 0;
+let g = 0;
+let b = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(r);
+  ellipse(mouseX, mouseY, mouseX / 5, mouseX / 5);
+  fill(255,g,b)
+}
+function mousePressed() {
+  if (r === 0) {
+    r = 255;
+  }
+  else {
+    r = 0;
+  }
 }
