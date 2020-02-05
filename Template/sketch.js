b335b91da779352737356d64d31247d187f1013f
tjhs1234 <60493051+tjhs1234@users.noreply.github.com>
2020-02-05 13:09:46 -0600

    Undid Changes

--- Template/sketch.js
+++ Template/sketch.js
@@ -1,48 +1,27 @@
-// Gravity Test
-// Thomas Schorr
-// February 4th, 2020
+// Project Title
+// Your Name
+// Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let r = 0;
+let g = 0;
+let b = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  let xPos = windowWidth / 2;
-  let yPos = 0;
-  let xVel = 0;
-  let yVel = 0;
-  let isTouchingGround = true;
-
-  let GRAVITY = 1;
-  let FLOORHEIGHT = 903;
-  
-  // Gravity Script
-  // Updates isTouchingGround
-  if (yPos >= 903) {
-    isTouchingGround = true;
-    yPos = 903;
-    yVel = 0;
-  }
-  else {
-    isTouchingGround = false;
-  }
-
-  // Applies Gravity
-  if (isTouchingGround === false) {
-    yVel =+ 1;
-  }
-
-  // Updates Positions
-  if (isTouchingGround === true) {
-    xPos =+ xVel;
-    yPos =+ yVel;
-  }
-  ellipse(xPos, yPos, 40, 40);
 }
 
 function draw() {
-  
-  background(180);
+  background(r);
+  ellipse(mouseX, mouseY, mouseX / 5, mouseX / 5);
+  fill(255,g,b)
 }
-
-
+function mousePressed() {
+  if (r === 0) {
+    r = 255;
+  }
+  else {
+    r = 0;
+  }
+}
