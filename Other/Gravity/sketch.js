acc355c0959a22d4c64c4138bb2c0ade6301d998
tjhs1234 <60493051+tjhs1234@users.noreply.github.com>
2020-02-06 17:09:41 -0600

    Fixed merging issues

--- Other/Gravity/sketch.js
+++ Other/Gravity/sketch.js
@@ -5,7 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x,y,xVel,yVel,isTouchingGround;
+<<<<<<< HEAD
+let x,y,xVel,yVel,isTouchingGround, stage;
+=======
+let x,y,xVel,yVel,isTouchingGround,currentBack,jumpsRemaining,jumpIsPrimed;
+>>>>>>> f77e562fd91d6ab980530aa394b3595d7c21847e
 
 //C//O//N//S//T//A//N//T//S//
 let GRAVITY = 1;
@@ -13,6 +17,7 @@ let FRICTION = 1;
 let STOPAMOUNT = 2; // The lowest velocity the player can have
 let JUMPHEIGHT = 20;
 let GROUNDHEIGHT = 200;
+let JUMPS = 25;
 //C//O//N//S//T//A//N//T//S//
 
 function setup() {
@@ -22,13 +27,25 @@ function setup() {
   xVel = 0;
   yVel = 0;
   isTouchingGround = false;
+<<<<<<< HEAD
+  stage = 1;
 }
 
 function draw() {
-  background(100);
+  background(135, 206, 235);
+=======
+  currentBack = 0;
+  jumpsRemaining = JUMPS;
+  jumpIsPrimed = true; // Detects whether the user has let go of the up arrow
+}
+
+function draw() {
+  updateStage();
+>>>>>>> f77e562fd91d6ab980530aa394b3595d7c21847e
   character(x, y);
   xMovement(FRICTION);
   yMovement(GRAVITY);
+  desert();
 }
 
 function character(x, y) {
@@ -40,6 +57,12 @@ function character(x, y) {
   ellipse(x + 9, y - 9, 10, 10); // Right Eye
   strokeWeight(2);
   line(x - 5, y + 10, x + 5, y + 10); // Mouth
+}
+
+function desert() {
+  fill(194, 178, 128);
+  strokeWeight(1);
+  rect(0, windowHeight - GROUNDHEIGHT + 1, windowWidth, windowHeight);
 }
 
 function xMovement(FRICTION_) {
@@ -69,22 +92,43 @@ function xMovement(FRICTION_) {
   }
   else if (x + xVel < 20) {
     x = windowWidth - 20;
+<<<<<<< HEAD
+    stage -= 1;
   }
   else if (x + xVel > windowWidth - 20) {
     x = 20;
+    stage += 1;
+=======
+    currentBack -= 1;
+  }
+  else if (x + xVel > windowWidth - 20) {
+    x = 20;
+    currentBack += 1;
+>>>>>>> f77e562fd91d6ab980530aa394b3595d7c21847e
   }
 }
 
 function yMovement(GRAVITY_) {
 
   // Listens for key inputs
-  if (keyIsDown(UP_ARROW) && isTouchingGround) {
-    yVel -= JUMPHEIGHT;
+  if (keyIsDown(UP_ARROW) && jumpsRemaining > 0 && jumpIsPrimed === true) {
+    yVel = -JUMPHEIGHT;
     isTouchingGround = false;
     y += 1;
-
+    jumpsRemaining -= 1;
+    jumpIsPrimed = false;
   }
-  
+
+  if (!keyIsDown(UP_ARROW)) {
+    jumpIsPrimed = true;
+  }
+
+  print(jumpsRemaining);
+  // Resets jumps
+  if (isTouchingGround === true) {
+    jumpsRemaining = JUMPS;
+  }
+
   // Applies gravity
   if (isTouchingGround === false) {
     yVel += GRAVITY_ * 1.3; 
@@ -98,6 +142,56 @@ function yMovement(GRAVITY_) {
     y = windowHeight - GROUNDHEIGHT - 20;
     yVel = 0;
     isTouchingGround = true;
+  }
+}
+
+function updateStage() {
+
+  // Uses the mouse's Y position to change the sky color
+  background(135, 206, 235);
+  //print(mouseY);
+
+  // Makes sure the stage variable is in bounds
+  if (currentBack === -1) {
+    currentBack = 3;
+  }
+  else if (currentBack === 4) {
+    currentBack = 0;
+  }
+  
+  // Desert Scene
+  if (currentBack === 0) { 
+    fill(194, 178, 128);
+    strokeWeight(1);
+    rect(0, windowHeight - GROUNDHEIGHT, windowWidth, windowHeight);
+    strokeWeight(2);
+    fill(35, 117, 67);
+    rect(80, windowHeight - GROUNDHEIGHT, 60, -150);
+    rect(500, windowHeight - GROUNDHEIGHT, 60, -150);
+    rect(1400, windowHeight - GROUNDHEIGHT, 60, -150);
+  }
+
+  // Forest Scene
+  if (currentBack === 1) { 
+    fill(96, 158, 56);
+    strokeWeight(1);
+    rect(0, windowHeight - GROUNDHEIGHT, windowWidth, windowHeight);
+  }
+  
+  // Grassland Scene
+  if (currentBack === 2) { 
+    fill(96, 128, 56);
+    strokeWeight(1);
+    rect(0, windowHeight - GROUNDHEIGHT, windowWidth, windowHeight);
+  }
+
+  // Arctic Scene
+  if (currentBack === 3) { 
+    fill(195, 203, 217);
+    strokeWeight(1);
+    rect(0, windowHeight - GROUNDHEIGHT, windowWidth, windowHeight);
+    fill(111, 122, 159);
+    rect(0, windowHeight - GROUNDHEIGHT + 30, windowWidth, windowHeight);
   }
 }
 
