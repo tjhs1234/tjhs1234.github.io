f77e562fd91d6ab980530aa394b3595d7c21847e
tjhs1234 <60493051+tjhs1234@users.noreply.github.com>
2020-02-06 14:10:17 -0600

    Added 4 boimes, double jumps

---
+++ Challenges/rotateLeft3.js
@@ -0,0 +1,4 @@
+function rotateLeft3(nums){
+  return [nums[1], nums[2], nums[0]];
+  
+}
--- Other/Gravity/sketch.js
+++ Other/Gravity/sketch.js
@@ -5,7 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x,y,xVel,yVel,isTouchingGround;
+let x,y,xVel,yVel,isTouchingGround,currentBack,jumpsRemaining,jumpIsPrimed;
 
 //C//O//N//S//T//A//N//T//S//
 let GRAVITY = 1;
@@ -13,6 +13,7 @@ let FRICTION = 1;
 let STOPAMOUNT = 2; // The lowest velocity the player can have
 let JUMPHEIGHT = 20;
 let GROUNDHEIGHT = 200;
+let JUMPS = 25;
 //C//O//N//S//T//A//N//T//S//
 
 function setup() {
@@ -22,10 +23,13 @@ function setup() {
   xVel = 0;
   yVel = 0;
   isTouchingGround = false;
+  currentBack = 0;
+  jumpsRemaining = JUMPS;
+  jumpIsPrimed = true; // Detects whether the user has let go of the up arrow
 }
 
 function draw() {
-  background(100);
+  updateStage();
   character(x, y);
   xMovement(FRICTION);
   yMovement(GRAVITY);
@@ -69,22 +73,35 @@ function xMovement(FRICTION_) {
   }
   else if (x + xVel < 20) {
     x = windowWidth - 20;
+    currentBack -= 1;
   }
   else if (x + xVel > windowWidth - 20) {
     x = 20;
+    currentBack += 1;
   }
 }
 
 function yMovement(GRAVITY_) {
 
   // Listens for key inputs
-  if (keyIsDown(UP_ARROW) && isTouchingGround) {
-    yVel -= JUMPHEIGHT;
+  if (keyIsDown(UP_ARROW) && jumpsRemaining > 0 && jumpIsPrimed === true) {
+    yVel = -JUMPHEIGHT;
     isTouchingGround = false;
     y += 1;
-
+    jumpsRemaining -= 1;
+    jumpIsPrimed = false;
   }
-  
+
+  if (!keyIsDown(UP_ARROW)) {
+    jumpIsPrimed = true;
+  }
+
+  print(jumpsRemaining);
+  // Resets jumps
+  if (isTouchingGround === true) {
+    jumpsRemaining = JUMPS;
+  }
+
   // Applies gravity
   if (isTouchingGround === false) {
     yVel += GRAVITY_ * 1.3; 
@@ -98,6 +115,56 @@ function yMovement(GRAVITY_) {
     y = windowHeight - GROUNDHEIGHT - 20;
     yVel = 0;
     isTouchingGround = true;
+  }
+}
+
+function updateStage() {
+
+  // Uses the mouse's Y position to change the sky color
+  background(135, 206, 235);
+  //print(mouseY);
+
+  // Makes sure the stage variable is in bounds
+  if (currentBack === -1) {
+    currentBack = 3;
+  }
+  else if (currentBack === 4) {
+    currentBack = 0;
+  }
+  
+  // Desert Scene
+  if (currentBack === 0) { 
+    fill(194, 178, 128);
+    strokeWeight(1);
+    rect(0, windowHeight - GROUNDHEIGHT, windowWidth, windowHeight);
+    strokeWeight(2);
+    fill(35, 117, 67);
+    rect(80, windowHeight - GROUNDHEIGHT, 60, -150);
+    rect(500, windowHeight - GROUNDHEIGHT, 60, -150);
+    rect(1400, windowHeight - GROUNDHEIGHT, 60, -150);
+  }
+
+  // Forest Scene
+  if (currentBack === 1) { 
+    fill(96, 158, 56);
+    strokeWeight(1);
+    rect(0, windowHeight - GROUNDHEIGHT, windowWidth, windowHeight);
+  }
+  
+  // Grassland Scene
+  if (currentBack === 2) { 
+    fill(96, 128, 56);
+    strokeWeight(1);
+    rect(0, windowHeight - GROUNDHEIGHT, windowWidth, windowHeight);
+  }
+
+  // Arctic Scene
+  if (currentBack === 3) { 
+    fill(195, 203, 217);
+    strokeWeight(1);
+    rect(0, windowHeight - GROUNDHEIGHT, windowWidth, windowHeight);
+    fill(111, 122, 159);
+    rect(0, windowHeight - GROUNDHEIGHT + 30, windowWidth, windowHeight);
   }
 }
 
