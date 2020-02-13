c23ea8f8bbef44826fea133a4840f394ecc042a6
tjhs1234 <60493051+tjhs1234@users.noreply.github.com>
2020-02-12 21:28:58 -0600

    Made the fade animation for the second assignment

--- Assignments/State Variables Assignment/sketch.js
+++ Assignments/State Variables Assignment/sketch.js
@@ -5,45 +5,106 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+const FADE_SPEED = 10;
+const MAX_RGB = 255;
 let mouseInQuadrant;
-
+let quad1Fade = MAX_RGB;
+let quad2Fade = MAX_RGB;
+let quad3Fade = MAX_RGB;
+let quad4Fade = MAX_RGB;
+let allFade = MAX_RGB;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function drawRect(one, two, three, four) {
-  strokeWeight(0)
-  fill(one);
-  rect(windowWidth, 0, -windowWidth / 2, windowHeight / 2 + 1);
-  fill(two);
-  rect(0, 0, windowWidth / 2, windowHeight / 2 + 1);
-  fill(three);
-  rect(0, windowHeight, windowWidth / 2, -windowHeight / 2);
-  fill(four);
-  rect(windowWidth, windowHeight, -windowWidth / 2, -windowHeight / 2);
+function drawRectangle(quadrant) {
+  strokeWeight(0);
+  fill(MAX_RGB);
+  if (quadrant === 1) {
+    if (quad1Fade - FADE_SPEED > 128) {
+      quad1Fade -= FADE_SPEED;
+    }
+    else {
+      quad1Fade = 128;
+    }
+    fill(quad1Fade);
+    rect(windowWidth, 0, -windowWidth / 2, windowHeight / 2 + 1);
+  }
+  else {
+    quad1Fade = MAX_RGB;
+  }
+  if (quadrant === 2) {
+    if (quad2Fade - FADE_SPEED > 128) {
+      quad2Fade -= FADE_SPEED;
+    }
+    else {
+      quad2Fade = 128;
+    }
+    fill(quad2Fade);
+    rect(0, 0, windowWidth / 2, windowHeight / 2 + 1);
+  }
+  else {
+    quad2Fade = MAX_RGB;
+  }
+  if (quadrant === 3) {
+    if (quad3Fade - FADE_SPEED > 128) {
+      quad3Fade -= FADE_SPEED;
+    }
+    else {
+      quad3Fade = 128;
+    }
+    fill(quad3Fade);
+    rect(0, windowHeight, windowWidth / 2, -windowHeight / 2);
+  }
+  else {
+    quad3Fade = MAX_RGB;
+  }
+  if (quadrant === 4) {
+    if (quad4Fade - FADE_SPEED > 128) {
+      quad4Fade -= FADE_SPEED;
+    }
+    else {
+      quad4Fade = 128;
+    }
+    fill(quad4Fade);
+    rect(windowWidth, windowHeight, -windowWidth / 2, -windowHeight / 2);
+  }
+  else {
+    quad4Fade = MAX_RGB;
+  }
+  if (quadrant === 5) {
+    fill(150);
+    rect(windowWidth, 0, -windowWidth / 2, windowHeight / 2 + 1);
+    rect(0, 0, windowWidth / 2, windowHeight / 2 + 1);
+    rect(0, windowHeight, windowWidth / 2, -windowHeight / 2);
+    rect(windowWidth, windowHeight, -windowWidth / 2, -windowHeight / 2);
+  }
+  else {
+    allFade = MAX_RGB;
+  }
 }
 
 function detectMouse() {
   if (mouseX >= windowWidth / 2 && mouseY <= windowHeight / 2) {
-    return 1;
+    mouseInQuadrant = 1;
   }
   else if (mouseX <= windowWidth / 2 && mouseY <= windowHeight / 2) {
-    return 2;
+    mouseInQuadrant = 2;
   }
   else if (mouseX <= windowWidth / 2 && mouseY >= windowHeight / 2) {
-    return 3;
+    mouseInQuadrant = 3;
   }
   else {
-    return 4;
+    mouseInQuadrant = 4;
   }
 }
 
 function draw() {
-  background(220);
-  print(detectMouse());
-  if (detectMouse() === 1) drawRect(100, 200, 200, 200);
-  else if (detectMouse() === 2) drawRect(200, 100, 200, 200);
-  else if (detectMouse() === 3) drawRect(200, 200, 100, 200);
-  else if (detectMouse() === 4) drawRect(200, 200, 200, 100);
+  background(MAX_RGB);
+  detectMouse();
+  drawRectangle(mouseInQuadrant);
+  strokeWeight(2);
+  line(0, windowHeight / 2, windowWidth, windowHeight / 2);
+  line(windowWidth / 2, 0, windowWidth / 2, windowHeight);
 }
