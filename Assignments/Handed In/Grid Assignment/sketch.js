709b1fb84b9664f7ade4fd5f9b2cbe6fd0df2f43
tjhs1234 <60493051+tjhs1234@users.noreply.github.com>
2020-03-03 21:08:05 -0600

    Handed in the grid assignment

--- Assignments/Handed In/Grid Assignment/sketch.js
+++ Assignments/Handed In/Grid Assignment/sketch.js
@@ -6,7 +6,7 @@
 // Made the rectangles automatically fit the screen
 // Added a few color presets
 
-let squaresInLine = 16;
+let squaresInLine = 16; // Number of squares in a horizontal line
 let squareMoveAmount = 0;
 let fillColor = [0,0,0];
 let windowRatio = 0;
@@ -14,10 +14,10 @@ let verticalSquaresInLine = 0;
 let verticalSquareMoveAmount = 0;
 
 //C//O//N//S//T//A//N//T//S//
-const COLOR_SCHEME = "red" ; // Color presets: red, green, blue, horizontalstripe, light
+const COLOR_SCHEME = "horizontalStripe" ; // Color presets: red, green, blue, horizontalstripe, light
 const INCREASE_VALUE = 2 ; // The amount to increase the rectangles, must be a perfect square
-const MAX_SQUARE_AMOUNT = 64 ; // Must be perfect square
-const MIN_SQUARE_AMOUNT = 2 ; // Must be perfect square
+const MAX_SQUARE_AMOUNT = 64 ; // Must be a perfect square
+const MIN_SQUARE_AMOUNT = 2 ; // Must be a perfect square
 const LINE_THICKNESS = 50 ;
 //C//O//N//S//T//A//N//T//S//
 
@@ -35,24 +35,26 @@ function keyPressed() {
 }
 
 function calculateValues() {
-  // Recalculates ratios in the event of the size changing
-  squareMoveAmount = windowWidth / squaresInLine;
-  windowRatio = height / width;
-  verticalSquaresInLine = squaresInLine * windowRatio;
-  verticalSquareMoveAmount = squareMoveAmount * windowRatio;
+  // Recalculates ratios in the event of the size of squares changing
+  squareMoveAmount = windowWidth / squaresInLine; // The horizontal size of a rectangle
+  windowRatio = height / width; // Ratio of horizontal pixels to vertical pixels
+  verticalSquaresInLine = squaresInLine * windowRatio; // Number of vertical squares per line
+  verticalSquareMoveAmount = squareMoveAmount * windowRatio; // The vertical size of rectangels
 }
 
 function mouseClicked() {
-  
+  // Changes the size of each squares
   if (mouseButton === LEFT && key === "Shift" && keyIsPressed && squaresInLine > MIN_SQUARE_AMOUNT) {
+    // Makes the squares bigger if shift clicked
     squaresInLine = squaresInLine / INCREASE_VALUE;
     calculateValues();
   }
-  
   else if (mouseButton === LEFT && !keyIsPressed && squaresInLine < MAX_SQUARE_AMOUNT) {
+    // Makes the squares smaller if normal clicked
     squaresInLine = squaresInLine * INCREASE_VALUE;
     calculateValues();
   }
+  // Redraws the squares each time the size is adjusted
   circleGrid();
 }
 
@@ -83,6 +85,7 @@ function setRandomColor(x_, y_, scheme) {
     fillColor[1] = 0;
     fillColor[2] = 255 - (x_ + y_) / 10;
   }
+  // Gives random values if no value or an unrecognized value is entered
   else {
     fillColor[0] = random(0, 255);
     fillColor[1] = random(0, 255);
@@ -91,9 +94,11 @@ function setRandomColor(x_, y_, scheme) {
 }
 
 function circleGrid() {
+  // Draws the rectangle grid
   if (squareMoveAmount !== 0) {
     for (let x = 0; x < width; x += squareMoveAmount) {
       for (let y = 0; y < height; y += verticalSquareMoveAmount) {
+        // Specific details for each rectangle
         strokeWeight(LINE_THICKNESS / squaresInLine);
         setRandomColor(x, y, COLOR_SCHEME);
         fill(fillColor[0], fillColor[1], fillColor[2]);
@@ -101,8 +106,4 @@ function circleGrid() {
       }
     }
   }
-}
-
-function draw() {
-  print(squaresInLine);
 }
