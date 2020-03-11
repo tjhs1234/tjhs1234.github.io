ee08758bf53e20ba92836bd9ca8734eb9a3ea19d
Schorr3401015 <3401015@sps-student.sk.ca>
2020-03-11 13:08:04 -0600

    Fixed merging issues

---
+++ Assignments/2D Perlin Noise/sketch.js
@@ -0,0 +1,62 @@
+// ̶ ̶M̶̶̶u̶̶̶l̶̶̶t̶̶̶i̶̶̶-̶̶̶C̶̶̶o̶̶̶l̶̶̶o̶̶̶u̶̶̶r̶̶̶e̶̶̶d̶̶̶ ̶̶̶G̶̶̶r̶̶̶i̶̶̶d̶̶̶ ̶̶̶A̶̶̶s̶̶̶s̶̶̶i̶̶̶g̶̶̶n̶̶̶m̶̶̶e̶̶̶n̶̶̶t̶̶̶  2D perlin noise
+// Thomas Schorr
+// March 10th, 2020
+
+let squaresInLine = 16; // Number of squares in a horizontal line
+let squareMoveAmount = 0;
+let fillColor = [0,0,0];
+let windowRatio = 0;
+let verticalSquaresInLine = 0;
+let verticalSquareMoveAmount = 0;
+
+//C//O//N//S//T//A//N//T//S//
+const INCREASE_VALUE = 2 ; // The amount to increase the rectangles, must be a perfect square
+const MAX_SQUARE_AMOUNT = 512 ; // Must be a perfect square
+const MIN_SQUARE_AMOUNT = 2 ; // Must be a perfect square
+const LINE_THICKNESS = 50 ;
+const MOVEMENT_DELTA = 250;
+//C//O//N//S//T//A//N//T//S//
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  calculateValues();
+  rectGrid();
+}
+
+function calculateValues() {
+  // Recalculates ratios in the event of the size of squares changing
+  squareMoveAmount = windowWidth / squaresInLine; // The horizontal size of a rectangle
+  windowRatio = height / width; // Ratio of horizontal pixels to vertical pixels
+  verticalSquaresInLine = squaresInLine * windowRatio; // Number of vertical squares per line
+  verticalSquareMoveAmount = squareMoveAmount * windowRatio; // The vertical size of rectangels
+}
+
+function mouseClicked() {
+  // Changes the size of each squares
+  if (mouseButton === LEFT && key === "Shift" && keyIsPressed && squaresInLine > MIN_SQUARE_AMOUNT) {
+    // Makes the squares bigger if shift clicked
+    squaresInLine = squaresInLine / INCREASE_VALUE;
+    calculateValues();
+  }
+  else if (mouseButton === LEFT && !keyIsPressed && squaresInLine < MAX_SQUARE_AMOUNT) {
+    // Makes the squares smaller if normal clicked
+    squaresInLine = squaresInLine * INCREASE_VALUE;
+    calculateValues();
+  }
+  // Redraws the squares each time the size is adjusted
+  rectGrid();
+}
+
+function rectGrid() {
+  // Draws the rectangle grid
+  if (squareMoveAmount !== 0) {
+    for (let x = 0; x < width; x += squareMoveAmount) {
+      for (let y = 0; y < height; y += verticalSquareMoveAmount) {
+        // Specific details for each rectangle
+        strokeWeight(LINE_THICKNESS / squaresInLine);
+        fill(map(noise(x / MOVEMENT_DELTA, y / MOVEMENT_DELTA), 0, 1, 0, 255));
+        rect(x,y,squareMoveAmount,verticalSquareMoveAmount);
+      }
+    }
+  }
+}
--- Assignments/Perlin Noise Assignment/sketch.js
+++ Assignments/Perlin Noise Assignment/sketch.js
@@ -3,85 +3,106 @@
 // March 6th, 2020
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Did the mountain top challenge
+// Did the average challenge
+// You can pan in both directions with your mouse
+// w and s change the time delta
+// a and d change the rectangle size  
 
-let currentRectangleHeight, time;
-
-let originalTime = 1000;
+let currentRectangleHeight = 0;
+let rectangleWidth = 1;
 let timeDelta = 0.01;
+let originalTime = 65536; // Starting position of camera. Should be far away from zero, since the terrain at zero is mirrored
 
-const RECTANGLE_WIDTH = 3;
-const PAN_DELTA = 0.1;
-const PAN_DEAD_ZONE = 500;
+//C//O//N//S//T//A//N//T//S//
+const PAN_DELTA = 0.3; // Speed the camera will pan at
+const DEAD_ZONE = 200; // Size of area in pixels the mouse can be without panning the camera
+const LOWER_TERRAIN_AMOUNT = 1.2; // Lowers the terrain so it doesn't take up so much of the screen
+const MAX_TIME_DELTA = 0.04;
+const MIN_TIME_DELTA = 0.00125;
+const MAX_RECTANGLE_WIDTH = 128;
+const MIN_RECTANGLE_WIDTH = 1;
+//C//O//N//S//T//A//N//T//S//
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CORNER);
   fill(0);
-
   currentRectangleHeight = 1;
   time = 0;
   strokeWeight(0);
 }
 
 function generateTerrain() {
+  // This function generates and draws the terrain, then calls
+  // the functions that render the average bar and flag
   fill(0);
-  time = originalTime;
-  let averageHeight = [0, 0];
-  let tallestPoint = [0, 0, 0, 0, 0, 0];
+  let time = originalTime; // This time variable will count with
+  let average = [width / rectangleWidth, 0]; // A list to calculate the average height. First position is number of rectangles, and second position is total rectangle height.
+  let tallestPoint = [0, 0]; // A list to calculate the tallest point. First position
 
-  for (let i = 0; i < width; i += RECTANGLE_WIDTH) {
-    currentRectangleHeight = map(noise(time), 0, 1, 0, height * 0.8);
+  for (let i = 0; i < width; i += rectangleWidth) {
+    currentRectangleHeight = map(noise(time), 0, 1, 0, height / LOWER_TERRAIN_AMOUNT); // Calculates the rectangle's height
     
-    rect(i, height, RECTANGLE_WIDTH, -currentRectangleHeight);
+    rect(i, height, rectangleWidth, -currentRectangleHeight); // Draws the rectangle
     
     time += timeDelta;
 
-    averageHeight[0] += 1;
-    averageHeight[1] += height - currentRectangleHeight;
-    if (currentRectangleHeight > tallestPoint[0]) {
+    average[1] += height - currentRectangleHeight; // Adds the rectangle's height to the total height variable
+    if (currentRectangleHeight > tallestPoint[0]) { // If the rectangle is the tallest one so far, it updates the tallestPoint list
       tallestPoint[0] = currentRectangleHeight;
       tallestPoint[1] = i;
     }
   }
+  // Draws the flag and average bar
   drawFlag(tallestPoint);
-  drawAverage(averageHeight);
+  drawAverage(average);
 }
 
-function drawAverage(averageHeight_) {
+function drawAverage(average_) {
   // Draws the average bar
   fill(50, 50, 225, 95);
-  rect(0, averageHeight_[1] / averageHeight_[0] + 5, width, -5);
-  print(averageHeight_);
+  rect(0, average_[1] / average_[0] + 5, width, -10 );
 }
 
 function drawFlag(tallestPoint_) {
-  // Draws the flag at the highest point
-  strokeWeight(1.5);
-  fill (150);
-  triangle(tallestPoint_[1] + RECTANGLE_WIDTH / 2, height - tallestPoint_[0] - 40, tallestPoint_[1] + RECTANGLE_WIDTH / 2, height - tallestPoint_[0] - 20, tallestPoint_[1] + 25, height - tallestPoint_[0] - 30);
+  // Draws the flag
+  fill(150,0,0);
+  strokeWeight(2);
+  triangle(tallestPoint_[1] + 5, height - tallestPoint_[0] - 40, tallestPoint_[1] + 5, height - tallestPoint_[0] - 20, tallestPoint_[1] + 25, height - tallestPoint_[0] - 30);
   fill(0);
-  rect(tallestPoint_[1] - RECTANGLE_WIDTH / 2, height - tallestPoint_[0] + 40, RECTANGLE_WIDTH / 2, -80);
+  rect(tallestPoint_[1] - 5, height - tallestPoint_[0] + 40, 10, -80);
   strokeWeight(0);
 }
 
 function panCamera() {
-  // Senses where the camera is and changes originalTime accordingly
-  if (mouseX < width / 2 - PAN_DEAD_ZONE / 2) {
-    originalTime -= PAN_DELTA;
+  // Calculates the amount of time to pan through taking into
+  // account the amount of time in view and the rectangle sizes
+  if (mouseX < width / 2 - DEAD_ZONE / 2) {
+    originalTime -= PAN_DELTA * (timeDelta / MAX_TIME_DELTA) / rectangleWidth;
   }
-  else if (mouseX > width / 2 + PAN_DEAD_ZONE / 2) {
-    originalTime += PAN_DELTA;
+  else if (mouseX > width / 2 + DEAD_ZONE / 2) {
+    originalTime += PAN_DELTA * (timeDelta / MAX_TIME_DELTA) / rectangleWidth;
   }
 }
 
 function keyPressed() {
-  // Changes the size of the terrain
-  if (key === "w") {
+  // Changes the time delta
+  if (key === "w" && timeDelta < MAX_TIME_DELTA) {
     timeDelta = timeDelta * 2;  
   }
-  else if (key === "s") {
+  else if (key === "s" && timeDelta > MIN_TIME_DELTA) {
     timeDelta = timeDelta / 2;
+  }
+  
+  // Changes the rectangle size
+  if (key === "a" && rectangleWidth < MAX_RECTANGLE_WIDTH) {
+    rectangleWidth = rectangleWidth * 2;
+    timeDelta = timeDelta * 2; // The time delta also needs to change here to account for the different time amounts being covered
+  }
+  else if (key === "d" && rectangleWidth > MIN_RECTANGLE_WIDTH) {
+    rectangleWidth = rectangleWidth / 2;
+    timeDelta = timeDelta / 2; // The time delta also needs to change here to account for the different time amounts being covered
   }
 }
 
